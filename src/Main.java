b533019afb7d7c10037e5fcf1b025f1c2a810a2b
Vladimir Semenov <osmyswait@gmail.com>
2022-10-30 11:09:19 +0300

    finalizing home work

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-        int balance = 3746; // начальный баланс на счету
+        int balance = 100; // начальный баланс на счету
         int pay = 1000; // платеж
-        int bonus = pay / 100; // бонус за пополнение счета
+
+        int bonus = pay > 1000 ? pay / 100 : 0; // бонус за пополнение счета
+
         int finalBalance = balance + pay + bonus; // итоговый баланс на счету
-
-        if (pay >= 1000) {
-            bonus = bonus;
-        } else {
-            bonus = 0;
-        }
-
 
         System.out.println("Ваш счет пополнен на сумму " + pay);
         System.out.println("Из них бонусами начислено " + bonus);
